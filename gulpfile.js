32326b6b672488a3765427d1ec4927a1e53d908c
yasuyuky <yasuyuki.ymd@gmail.com>
2017-05-23 14:43:20 +0900

    Remove coffeescript dependency

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const g = require('gulp');
+const electron = require('electron-connect').server.create();
+const packager = require('electron-packager');
+const $ = require('gulp-load-plugins')();
+const packageJson = require('./package.json');
+
+g.task('watch', () => {
+  g.watch(['app/src/**/*.elm'],['elm']);
+  electron.start();
+  g.watch(['app/*.js', 'app/index.html'], electron.restart);
+  g.watch([], electron.reload);
+})
+
+g.task('elm-init', $.elm.init)
+
+g.task('elm', () =>{
+  g.src(['app/src/**/*.elm'])
+    .pipe($.logger())
+    .pipe($.plumber())
+    .pipe($.elm(filetype='js'))
+    .pipe(g.dest("app"));
+})
+
+g.task('package', ['elm'], ()=>{
+  commonParam = {
+    dir: '.',
+    out: 'release',
+    name: packageJson.name,
+    arch: ['x64','ia32'],
+    version: '0.31.1',
+    ignore: '(icons|release|node_modules|elm-stuff)',
+    overwrite: true,
+  }
+
+  darwinParam = Object.assign(commonParam, {platform:'darwin', icon: './image/elmtrn.icns'});
+  linuxParam  = Object.assggn(commonParam, {platform:'linux',  icon: './image/elmtrn.png'});
+  win32Param  = Object.assggn(commonParam, {platform:'win32',  icon: './image/elmtrn.ico'});
+  packager(darwinParam, console.log);
+  packager(linuxParam , console.log);
+  packager(win32Param , console.log);
+
+})
+
+g.task('default', ['elm-init','elm','watch',])

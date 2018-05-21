d36247a89f4a10a648d169f78dc93f6cf3e944b2
yasuyuky <yasuyuki.ymd@gmail.com>
2018-05-21 12:55:03 +0900

    Format with prettier

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-var {app, BrowserWindow} = require('electron');
+var { app, BrowserWindow } = require("electron");
 var mainWindow = null;
 
-app.on('window-all-closed', function() {
-    app.quit();
+app.on("window-all-closed", function() {
+  app.quit();
 });
 
-app.on('ready', function() {
+app.on("ready", function() {
   mainWindow = new BrowserWindow({
-    "width": 225,
-    "height": 225,
-    "transparent": true,
-    "frame": false,
-    "alwaysOnTop": true,
-    "resizable": false
+    width: 225,
+    height: 225,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    resizable: false
   });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.on('closed', function() {
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+  mainWindow.on("closed", function() {
     mainWindow = null;
   });
 });
